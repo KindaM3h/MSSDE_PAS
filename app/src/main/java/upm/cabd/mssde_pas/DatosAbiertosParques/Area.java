01d7e2922401ddcaff07c7e4de30bff331afca63
cbarreiro <carlosbarreirodiaz@hotmail.com>
2023-06-02 16:01:27 +0200

    Add generated Plain Old Java Objects from JSON and GSON dependency targeting https://datos.madrid.es/egob/catalogo/200761-0-parques-jardines.json

---
+++ app/src/main/java/upm/cabd/mssde_pas/DatosAbiertosParques/Area.java
@@ -0,0 +1,21 @@
+
+package upm.cabd.mssde_pas.DatosAbiertosParques;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Area {
+
+    @SerializedName("@id")
+    @Expose
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
---
+++ app/src/main/java/upm/cabd/mssde_pas/DatosAbiertosParques/District.java
@@ -0,0 +1,20 @@
+
+package upm.cabd.mssde_pas.DatosAbiertosParques;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+public class District {
+
+    @SerializedName("@id")
+    @Expose
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
